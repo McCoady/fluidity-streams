02bdac261f3e2dc8d85873b3927f1c7dbec3d074
McCoady <78250146+McCoady@users.noreply.github.com>
2021-09-27 01:19:54 +0900

    Contract Basics

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,22 +1,49 @@
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity 0.8.4;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
-contract YourContract {
+import "hardhat/console.sol";
 
-  //event SetPurpose(address sender, string purpose);
+import {ISuperfluid, ISuperToken, ISuperApp, ISuperAgreement, SuperAppDefinitions} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol"; //"@superfluid-finance/ethereum-monorepo/packages/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 
-  string public purpose = "Building Unstoppable Apps!!!";
+import {IConstantFlowAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
 
-  constructor() {
-    // what should we do on deploy?
-  }
+import {IInstantDistributionAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IInstantDistributionAgreementV1.sol";
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      //emit SetPurpose(msg.sender, purpose);
-  }
+import {SuperAppBase} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+
+contract YourContract is SuperAppBase {
+    ISuperfluid host; // Superfluid host contract
+    IConstantFlowAgreementV1 cfa; // The stored constant flow agreement class address
+    IInstantDistributionAgreementV1 ida; // The stored instant dist. agreement class address
+    ISuperToken inputToken; // The input token (e.g. DAIx)
+    ISuperToken outputToken; // The output token (e.g. ETHx)
+
+    constructor(
+        ISuperfluid _host,
+        IConstantFlowAgreementV1 _cfa,
+        IInstantDistributionAgreementV1 _ida,
+        ISuperToken _inputToken,
+        ISuperToken _outputToken
+    ) {
+        host = _host;
+        cfa = _cfa;
+        ida = _ida;
+        inputToken = _inputToken;
+        outputToken = _outputToken;
+        uint256 configWord = SuperAppDefinitions.APP_LEVEL_FINAL |
+            SuperAppDefinitions.BEFORE_AGREEMENT_CREATED_NOOP |
+            SuperAppDefinitions.BEFORE_AGREEMENT_UPDATED_NOOP |
+            SuperAppDefinitions.BEFORE_AGREEMENT_TERMINATED_NOOP;
+        _host.registerApp(configWord);
+    }
+
+    function getNetFlow() public view returns (int96) {
+        return _cfa.getNetFlow(_acceptedToken, address(this));
+    }
 }
